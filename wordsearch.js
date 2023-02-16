ac381bbc3ed02cb4410673b06fb00baa2208479b
Nima <nima.mohamed@hotmail.com>
2023-02-15 17:57:34 -0800

    Pair Programin W2 D3

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if (word === "" || Array.isArray(word)) return false;
+  
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (let l of horizontalJoin) {
+      
+      if (l.toLowerCase().includes(word.toLowerCase())) return true;
     }
-}
-
-module.exports = wordSearch
+  
+    const transpose = function(matrix) {
+      const newMatrix = [];
+      for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+          if (i === 0) newMatrix.push([matrix[i][j]]);
+          if (i > 0) newMatrix[j].push(matrix[i][j]);
+        }
+      }
+      return newMatrix;
+    };
+  
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    for (let l of verticalJoin) {
+      if (l.toLowerCase().includes(word.toLowerCase())) return true;
+    }
+    return false;
+  };
+  
+  module.exports = wordSearch;
